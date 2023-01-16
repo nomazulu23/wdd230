e9c6073b3bf3e9b902dd0bc22dd218d3d18b7f45
Heather Jasi <u033813021@byui.edu>
2023-01-17 00:59:27 +0200

    Week 1 Final Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-//const year = document.querySelector('#year');
-//year.textContent= `${newDate().getFullYear()}`;
+
 
 
 // Select the HTML element to manipulate
@@ -14,7 +13,7 @@ try {
 		month: "long",
 		year: "numeric"
 	};
-	date1.innerHTML = `Today is <span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>!`;
+	date1.innerHTML = `Last Updated <span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>!`;
 } catch (e) {
 	console.log("Error with code or your browser does not support Locale");
 }
@@ -47,5 +46,5 @@ const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+const fulldate = `${dayName},  ${d.getDate()} ${monthName} ${year}`;
 document.querySelector("#date2").textContent = fulldate;
