e6e4022cd5d82418d1a301077c0d772db8cec2cf
Heather Jasi <u033813021@byui.edu>
2023-01-21 01:04:08 +0200

    Design Initial Commit

---
+++ lesson_2/scripts2/design.js
@@ -0,0 +1,10 @@
+const date = new Date();
+// current year
+const year = date.getFullYear();
+document.querySelector("#year").innerHTML = year;
+
+let dateModified = ('Last updated: ' + document.lastModified);
+document.getElementById("date").innerHTML = dateModified;
+
+
+//Source: https://www.freecodecamp.org/news/javascript-get-current-date-todays-date-in-js/
