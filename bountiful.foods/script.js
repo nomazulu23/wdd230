ae6fb14775058e955c8c4e2a5371a2f30d15e888
Heather Jasi <u033813021@byui.edu>
2023-04-06 12:58:30 +0200

    second commit

---
+++ bountiful.foods/script.js
@@ -0,0 +1,12 @@
+var currentDate = new Date();
+var day = currentDate.getDay();
+var dayNum = currentDate.getDate();
+var month = currentDate.getMonth();
+var year = currentDate.getFullYear();
+
+var days = ["Sunday","Monday","Tuesday"," Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+var dateString = days[day] + ", " + dayNum + " " + months[month] + " " + year;
+
+document.getElementById("get_date").innerHTML = dateString;
